39e1b2e4d21dcc72e6e015f30fbdeec8daf7268e
Stefano Frontini <stefanofrontini75@gmail.com>
2023-10-09 23:25:12 +0200

    connections

--- src/components/lib/visualizer.ts
+++ src/components/lib/visualizer.ts
@@ -22,18 +22,45 @@ class Visualizer {
     const right = left + width;
     const bottom = top + height;
 
+    const { inputs, outputs } = level;
+    for (let i = 0; i < inputs.length; i++) {
+      for (let j = 0; j < outputs.length; j++) {
+        ctx.beginPath();
+        ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom);
+        ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "orange";
+        ctx.stroke();
+      }
+    }
     const nodeRadius = 18;
-    for (let i = 0; i < level.inputs.length; i++) {
-      const x = lerp(
-        left,
-        right,
-        level.inputs.length === 1 ? 0.5 : i / (level.inputs.length - 1)
-      );
+    for (let i = 0; i < inputs.length; i++) {
+      const x = Visualizer.#getNodeX(inputs, i, left, right);
       ctx.beginPath();
       ctx.arc(x, bottom, nodeRadius, 0, 2 * Math.PI);
       ctx.fillStyle = "white";
       ctx.fill();
     }
+    for (let i = 0; i < outputs.length; i++) {
+      const x = Visualizer.#getNodeX(outputs, i, left, right);
+
+      ctx.beginPath();
+      ctx.arc(x, top, nodeRadius, 0, 2 * Math.PI);
+      ctx.fillStyle = "white";
+      ctx.fill();
+    }
+  }
+  static #getNodeX(
+    nodes: number[],
+    index: number,
+    left: number,
+    right: number
+  ) {
+    return lerp(
+      left,
+      right,
+      nodes.length === 1 ? 0.5 : index / (nodes.length - 1)
+    );
   }
 }
 export default Visualizer;
